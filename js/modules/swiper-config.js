2679187a1339d16eb0c67eb71fa5038136e370c7
Jobs <joabsonguitar@gmail.com>
2025-08-22 21:29:48 -0300

    chore: ideacao

--- js/modules/swiper-config.js
+++ js/modules/swiper-config.js
@@ -3,6 +3,7 @@ import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs
 const swiper = new Swiper(".swiper", {
   slidesPerView: 1,
   allowTouchMove: true,
+  initialSlide: 1,
 });
 
 const buttons = document.querySelectorAll(".bottom-navigation button");
